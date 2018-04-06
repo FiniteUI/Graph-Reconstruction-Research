f1e73d8354ce3a787ae21b385ffba41342e42c5b
Richard William Seddon III <tsed@ymail.com>
2018-04-06 16:12:31 -0500

    Add files via upload

--- D8 Stuff/AllCards(D8).py
+++ D8 Stuff/AllCards(D8).py
@@ -1,248 +1,156 @@
-<!DOCTYPE HTML>
-<html>
+#note: most of these are very specific and build off of eachother
+load("G800-G804.py")
+L = getD8Graphs()
+load('BD_Decks(D8).py')
 
-<head>
-    <meta charset="utf-8">
-
-    <title>AllCards(D8).py (editing)</title>
-    <link rel="shortcut icon" type="image/x-icon" href="/static/base/images/favicon.ico?v=97c6417ed01bdc0ae3ef32ae4894fd03">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <link rel="stylesheet" href="/static/components/jquery-ui/themes/smoothness/jquery-ui.min.css?v=9b2c8d3489227115310662a343fce11c" type="text/css" />
-    <link rel="stylesheet" href="/static/components/jquery-typeahead/dist/jquery.typeahead.min.css?v=7afb461de36accb1aa133a1710f5bc56" type="text/css" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
-    
-<link rel="stylesheet" href="/static/components/codemirror/lib/codemirror.css?v=f25e9a9159e54b423b5a8dc4b1ab5c6e">
-<link rel="stylesheet" href="/static/components/codemirror/addon/dialog/dialog.css?v=c89dce10b44d2882a024e7befc2b63f5">
-
-    <link rel="stylesheet" href="/static/style/style.min.css?v=974839a888beb55bbba87883fafd90fa" type="text/css"/>
-    
-
-    <link rel="stylesheet" href="/custom/custom.css" type="text/css" />
-    <script src="/static/components/es6-promise/promise.min.js?v=f004a16cb856e0ff11781d01ec5ca8fe" type="text/javascript" charset="utf-8"></script>
-    <script src="/static/components/requirejs/require.js?v=6da8be361b9ee26c5e721e76c6d4afce" type="text/javascript" charset="utf-8"></script>
-    <script>
-      require.config({
+def getAllCards():
+    #this returns a list of all cards as graph objects, with one BD_Decks in each cell
+    AllCards = []
+    for i in range(0, len(L)):
+        x = BD_Decks(L[i])
+        AllCards.append(x)
+    return AllCards
+        
+def makeList():
+    #this builds a list of strings representing all cards
+    MainList = []
+    KocayList = []
+    BigOleCardList = []
+    for i in range(0, len(L)):
+        p = []
+        p.append(L[i].name())
+        p.append(L[i].graph6_string())
+        p.append(L[i].degree_sequence())
+        MainList.append(p)
+        x = BD_Decks(L[i])
+        for j in x:
+            if type(j) is not sage.graphs.graph.Graph:
+                z = []
+                for k in j:
+                    y = []
+                    y.append(k.name())
+                    y.append(k.graph6_string())
+                    y.append(k.degree_sequence())
+                    z.append(y)
+                BigOleCardList.append(z)
+            else:
+                z = []
+                z.append(j.name())
+                z.append(j.graph6_string())
+                z.append(j.degree_sequence())
+                KocayList.append(z)
+    return MainList, KocayList, BigOleCardList       
+         
           
-          baseUrl: '/static/',
-          paths: {
-            'auth/js/main': 'auth/js/main.min',
-            custom : '/custom',
-            nbextensions : '/nbextensions',
-            kernelspecs : '/kernelspecs',
-            underscore : 'components/underscore/underscore-min',
-            backbone : 'components/backbone/backbone-min',
-            jquery: 'components/jquery/jquery.min',
-            bootstrap: 'components/bootstrap/js/bootstrap.min',
-            bootstraptour: 'components/bootstrap-tour/build/js/bootstrap-tour.min',
-            'jquery-ui': 'components/jquery-ui/ui/minified/jquery-ui.min',
-            moment: 'components/moment/moment',
-            codemirror: 'components/codemirror',
-            termjs: 'components/xterm.js/dist/xterm',
-            typeahead: 'components/jquery-typeahead/dist/jquery.typeahead.min',
-          },
-	  map: { // for backward compatibility
-	    "*": {
-		"jqueryui": "jquery-ui",
-	    }
-	  },
-          shim: {
-            typeahead: {
-              deps: ["jquery"],
-              exports: "typeahead"
-            },
-            underscore: {
-              exports: '_'
-            },
-            backbone: {
-              deps: ["underscore", "jquery"],
-              exports: "Backbone"
-            },
-            bootstrap: {
-              deps: ["jquery"],
-              exports: "bootstrap"
-            },
-            bootstraptour: {
-              deps: ["bootstrap"],
-              exports: "Tour"
-            },
-            "jquery-ui": {
-              deps: ["jquery"],
-              exports: "$"
-            }
-          },
-          waitSeconds: 30,
-      });
+def printCardList(BigOleCardList):
+    #this prints the list from MakeList in an easier to view fashion
+    print('Name\t\t\tgraph6\t\tDegree Sequence')
+    print("-------------------------------------------------------")
+    for i in BigOleCardList:
+        for j in i:
+            print
+            for k in j:
+                print(str(k) + "\t\t"),
+                
+def getUniqueCards(BigOleCardList):
+    #This makes a list of the graph6 notation of each unique card
+    AlreadySeenCard = []
+    for i in BigOleCardList:
+        for j in i:
+            match = False
+            g = Graph(j[1])
+            for k in range(0, len(AlreadySeenCard)):
+                f = Graph(AlreadySeenCard[k])
+                if f.is_isomorphic(g):
+                    match = True
+                    break;
+            if not match:
+                AlreadySeenCard.append(j[1])
+    return AlreadySeenCard 
 
-      require.config({
-          map: {
-              '*':{
-                'contents': 'services/contents',
-              }
-          }
-      });
+def MakeIsoList(AlreadySeenCard, BigOleCardList):
+    #this groups together all isomorphic cards
+    UniqueCardList = []
+    doneWith = []
+    for i in range(0, len(AlreadySeenCard)):
+        y = []
+        y.append(AlreadySeenCard[i])
+        g = Graph(AlreadySeenCard[i])
+        for j in BigOleCardList:
+            for k in j:
+                if k[0] not in doneWith:
+                    f = Graph(k[1])
+                    if f.is_isomorphic(g):
+                        y.append(k[0])
+                        doneWith.append(k[0])
+        UniqueCardList.append(y)
+    return UniqueCardList
 
-      define("bootstrap", function () {
-          return window.$;
-      });
+def printIsoList(UniqueCardList):
+    #this prints the iso list in a slightly prettier fashion
+    print "Isomorphisms:"
+    print
+    for i in UniqueCardList:
+        print('\t' + i[0])
+        for j in range(1, len(i)):
+            print('\t\t' + i[j])
+        print
+        
+def getDegreeLists(ISO):
+    #this organizes the graphs from the isolist by degree sequences
+    DSL = [[3,3,3,3,3,3,2,2,2], [], [3,3,3,3,3,3,2,1,1], [], [3,3,3,3,3,2,2,2,1], [], [3,3,3,3,2,2,2,2,2], []]
+    for i in ISO:
+        g = Graph(i[0])
+        if g.degree_sequence() == DSL[0]:
+            DSL[1].append(i)
+        elif g.degree_sequence() == DSL[2]:
+            DSL[3].append(i)
+        elif g.degree_sequence() == DSL[4]:
+            DSL[5].append(i)
+        elif g.degree_sequence() == DSL[6]:
+            DSL[7].append(i)
+        else:
+            print "Error: Unexpected degree sequence..."
+            break
+    return DSL
 
-      define("jquery", function () {
-          return window.$;
-      });
+def printDegreeLists(DSL):
+    #this prints the degree lists generated in the last function
+    for i in range(0, len(DSL)):
+        if i%2 == 0:
+            print DSL[i]
+        else:
+            for j in range(0, len(DSL[i])):
+                print "\t" + str(DSL[i][j][0])
+                for k in range(1, len(DSL[i][j])):
+                    print "\t\t" + str(DSL[i][j][k])
+                print
+            print "-------------------------------"
 
-      define("jqueryui", function () {
-          return window.$;
-      });
+def getKocayIso(K):
+    #this gets the isomorphism list for the kocay graphs
+    done = []
+    KISOList = []
+    for i in K:
+        KISOTemp = []
+        g = Graph(i[1])
+        if i[1] not in done:
+            KISOTemp.append(i[1])
+            KISOTemp.append(i[0])
+            done.append(g.graph6_string())
+            for j in K:
+                if j[1] not in done:
+                    f = Graph(j[1])
+                    if f.is_isomorphic(g):
+                        KISOTemp.append(j[0])
+                        done.append(j[1])
+            KISOList.append(KISOTemp)
+    return KISOList
 
-      define("jquery-ui", function () {
-          return window.$;
-      });
-      // error-catching custom.js shim.
-      define("custom", function (require, exports, module) {
-          try {
-              var custom = require('custom/custom');
-              console.debug('loaded custom.js');
-              return custom;
-          } catch (e) {
-              console.error("error loading custom.js", e);
-              return {};
-          }
-      })
-    </script>
-
-    
-    
-
-</head>
-
-<body class="edit_app "
- 
-data-base-url="/"
-data-file-path="AllCards%28D8%29.py"
-
-  
- 
-
->
-
-<noscript>
-    <div id='noscript'>
-      Jupyter Notebook requires JavaScript.<br>
-      Please enable it to proceed.
-  </div>
-</noscript>
-
-<div id="header">
-  <div id="header-container" class="container">
-  <div id="ipython_notebook" class="nav navbar-brand pull-left"><a href="/tree" title='dashboard'><img src='/static/base/images/logo.png?v=641991992878ee24c6f3826e81054a0f' alt='Jupyter Notebook'/></a></div>
-
-  
-  
-  
-
-    <span id="login_widget">
-      
-    </span>
-
-  
-
-  
-
-  
-
-<span id="save_widget" class="pull-left save_widget">
-    <span class="filename"></span>
-    <span class="last_modified"></span>
-</span>
-
-
-  </div>
-  <div class="header-bar"></div>
-
-  
-
-<div id="menubar-container" class="container">
-  <div id="menubar">
-    <div id="menus" class="navbar navbar-default" role="navigation">
-      <div class="container-fluid">
-          <p  class="navbar-text indicator_area">
-          <span id="current-mode" >current mode</span>
-          </p>
-        <button type="button" class="btn btn-default navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-          <i class="fa fa-bars"></i>
-          <span class="navbar-text">Menu</span>
-        </button>
-        <ul class="nav navbar-nav navbar-right">
-          <li id="notification_area"></li>
-        </ul>
-        <div class="navbar-collapse collapse">
-          <ul class="nav navbar-nav">
-            <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">File</a>
-              <ul id="file-menu" class="dropdown-menu">
-                <li id="new-file"><a href="#">New</a></li>
-                <li id="save-file"><a href="#">Save</a></li>
-                <li id="rename-file"><a href="#">Rename</a></li>
-                <li id="download-file"><a href="#">Download</a></li>
-              </ul>
-            </li>
-            <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">Edit</a>
-              <ul id="edit-menu" class="dropdown-menu">
-                <li id="menu-find"><a href="#">Find</a></li>
-                <li id="menu-replace"><a href="#">Find &amp; Replace</a></li>
-                <li class="divider"></li>
-                <li class="dropdown-header">Key Map</li>
-                <li id="menu-keymap-default"><a href="#">Default<i class="fa"></i></a></li>
-                <li id="menu-keymap-sublime"><a href="#">Sublime Text<i class="fa"></i></a></li>
-                <li id="menu-keymap-vim"><a href="#">Vim<i class="fa"></i></a></li>
-                <li id="menu-keymap-emacs"><a href="#">emacs<i class="fa"></i></a></li>
-              </ul>
-            </li>
-            <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">View</a>
-              <ul id="view-menu" class="dropdown-menu">
-              <li id="toggle_header" title="Show/Hide the logo and notebook title (above menu bar)">
-              <a href="#">Toggle Header</a></li>
-              <li id="menu-line-numbers"><a href="#">Toggle Line Numbers</a></li>
-              </ul>
-            </li>
-            <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">Language</a>
-              <ul id="mode-menu" class="dropdown-menu">
-              </ul>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
-
-<div class="lower-header-bar"></div>
-
-
-</div>
-
-<div id="site">
-
-
-<div id="texteditor-backdrop">
-<div id="texteditor-container" class="container"></div>
-</div>
-
-
-</div>
-
-
-
-
-
-
-    
-
-
-
-    <script src="/static/edit/js/main.min.js?v=3f26c490a03e64a774b6f266300dac6e" type="text/javascript" charset="utf-8"></script>
-
-
-
-</body>
-
-</html>
+def printKisoList(KISOList):
+    #this prints the kisolist generated in the last function
+    for i in KISOList:
+        print i[0]
+        for j in range(1, len(i)):
+            print '\t' + str(i[j])
+        print
